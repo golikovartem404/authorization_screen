e06408e6e52ab2e717c0c524d80d7f6197104a88
golikovartyom <golikovartem404@gmail.com>
2022-08-19 09:03:52 +0400

    Add extensions and login and password textField

--- authorization_screen/ViewController.swift
+++ authorization_screen/ViewController.swift
@@ -30,6 +30,31 @@ class ViewController: UIViewController {
         return loginLabel
     }()
 
+    private lazy var loginTextField: UITextField = {
+        let loginTextField = UITextField()
+        loginTextField.backgroundColor = .white
+        loginTextField.placeholder = "Login"
+        loginTextField.textAlignment = .left
+        loginTextField.clipsToBounds = true
+        loginTextField.layer.cornerRadius = 20
+        loginTextField.translatesAutoresizingMaskIntoConstraints = false
+        loginTextField.setLeftIcon(UIImage(named: "user") ?? UIImage(imageLiteralResourceName: ""))
+        loginTextField.setRightIcon(UIImage(named: "approve") ?? UIImage(imageLiteralResourceName: ""))
+        return loginTextField
+    }()
+
+    private lazy var passTextField: UITextField = {
+        let passTextField = UITextField()
+        passTextField.backgroundColor = .white
+        passTextField.placeholder = "Password"
+        passTextField.textAlignment = .left
+        passTextField.clipsToBounds = true
+        passTextField.layer.cornerRadius = 20
+        passTextField.translatesAutoresizingMaskIntoConstraints = false
+        passTextField.setLeftIcon(UIImage(named: "password") ?? UIImage(imageLiteralResourceName: ""))
+        return passTextField
+    }()
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -43,6 +68,8 @@ class ViewController: UIViewController {
     private func  setupHierarchy() {
         view.insertSubview(imageView, at: 0)
         view.addSubview(loginLabel)
+        view.addSubview(loginTextField)
+        view.addSubview(passTextField)
     }
 
 
@@ -60,6 +87,20 @@ class ViewController: UIViewController {
         loginLabel.snp.makeConstraints { make in
             make.centerX.equalTo(view)
             make.top.equalTo(view.snp.centerY).multipliedBy(0.2)
+        }
+
+        loginTextField.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.top.equalTo(loginLabel.snp.centerY).multipliedBy(1.7)
+            make.width.equalTo(view.snp.width).multipliedBy(0.8)
+            make.height.equalTo(40)
+        }
+
+        passTextField.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.top.equalTo(loginTextField.snp.centerY).multipliedBy(1.2)
+            make.width.equalTo(view.snp.width).multipliedBy(0.8)
+            make.height.equalTo(40)
         }
 
     }
