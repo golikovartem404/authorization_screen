734ffccce1f8c8f5c4e40ce107a810a278939995
golikovartyom <golikovartem404@gmail.com>
2022-08-19 08:55:51 +0400

    Add SnapKit and backgroundImage

--- authorization_screen/ViewController.swift
+++ authorization_screen/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupHierarchy()
+        setupLayouts()
     }
 
+    // MARK: - SetupHierarchy
+
+    private func  setupHierarchy() {
+        view.insertSubview(imageView, at: 0)
+    }
+
+
+    // MARK: - SetupLayouts
+
+    private func setupLayouts() {
+
+        imageView.snp.makeConstraints { make in
+            make.top.equalTo(view)
+            make.left.equalTo(view)
+            make.right.equalTo(view)
+            make.bottom.equalTo(view)
+        }
+
+    }
 
 }
 
